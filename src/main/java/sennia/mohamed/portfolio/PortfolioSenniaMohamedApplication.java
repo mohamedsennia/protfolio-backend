2147645f123984409d65d6ce0499dda6c955fbef
mohamedsennia <mohamedsennia@gmail.com>
2024-05-27 18:48:54 +0100

    jar file generated

--- src/main/java/sennia/mohamed/portfolio/PortfolioSenniaMohamedApplication.java
+++ src/main/java/sennia/mohamed/portfolio/PortfolioSenniaMohamedApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class 	PortfolioSenniaMohamedApplication extends SpringBootServletInitializer {
+public class 	PortfolioSenniaMohamedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioSenniaMohamedApplication.class, args);
